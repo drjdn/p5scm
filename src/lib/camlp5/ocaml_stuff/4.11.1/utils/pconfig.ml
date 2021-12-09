789f2eee9d5a0564facfe92d0661ffe3d5792bb2
Jason Nielsen <drjdnielsen@gmail.com>
2021-12-09 14:59:14 -0500

    Doing the opam-repository shuffle

--- src/lib/camlp5/ocaml_stuff/4.11.1/utils/pconfig.ml
+++
@@ -1,2 +0,0 @@
-let ast_impl_magic_number = "Caml1999M028"
-let ast_intf_magic_number = "Caml1999N028"
--- src/lib/camlp5/ocaml_stuff/4.12.1/utils/pconfig.ml
+++
@@ -1,2 +0,0 @@
-let ast_impl_magic_number = "Caml1999M029"
-let ast_intf_magic_number = "Caml1999N029"

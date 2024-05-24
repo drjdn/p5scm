b51545cde03efc24d2d74dad12ac59db4d648fbf
Jason Nielsen <drjdnielsen@gmail.com>
2024-05-24 14:12:20 -0400

    Working for OCaml >= 5

--- src/lib/camlp5/get_version.ml
+++ src/lib/camlp5/get_version.ml
@@ -1,6 +1,10 @@
 let get_version () =
   let vers = Stdlib.Sys.ocaml_version in
-  let vers0 = (String.sub vers 0 5) ^ "X" in
-  Printf.printf "%s\n" vers0; flush stdout
-
+  if (String.length vers) > 5 then
+    let vers0 = (String.sub vers 0 5) ^ "X" in
+    Printf.printf "%s\n" vers0; flush stdout
+  else
+    let vers0 = (String.sub vers 0 4) ^ "X" in
+    Printf.printf "%s\n" vers0; flush stdout
+    
 let _ = get_version ()

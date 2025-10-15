41eaf424dea3aac82c315c2e646c655a64f29703
Jason Nielsen <drjdnielsen@gmail.com>
2025-10-15 17:06:12 -0400

    More robost version of get_version()

--- src/lib/camlp5/get_version.ml
+++ src/lib/camlp5/get_version.ml
@@ -1,10 +1,7 @@
 let get_version () =
   let vers = Stdlib.Sys.ocaml_version in
-  if (String.length vers) > 5 then
-    let vers0 = (String.sub vers 0 5) ^ "X" in
-    Printf.printf "%s\n" vers0; flush stdout
-  else
-    let vers0 = (String.sub vers 0 4) ^ "X" in
-    Printf.printf "%s\n" vers0; flush stdout
+  let indx = String.index_from vers 2 '.' in 
+  let vers0 = (String.sub vers 0 (indx+1)) ^ "X" in
+  Printf.printf "%s\n" vers0; flush stdout
     
 let _ = get_version ()
